ba4b4950d8b0f622b5b4ff53d246cf656205f1a9
Nyatsume <xynaoyuki@gmail.com>
2020-01-03 00:16:21 +0900

    added check flag

--- main.c
+++ main.c
@@ -11,3 +11,50 @@
 #include <arpa/inet.h>
 #include <sys/wait.h>
 #include <pthread.h>
+#include "sock.h"
+#include "ether.h"
+#include "arp.h"
+#include "icmp.h"
+#include "param.h"
+#include "cmd.h"
+
+int EndFlag=0;
+
+int DeviceSoc;
+
+PARAM param;
+
+void *MyEthThread(void *arg)
+{
+    int nready;
+    struct pollfd targets[1];
+    u_int8_t buf[2048];
+    int len;
+
+    targets[0].fd=DeviceSoc;
+    targets[0].events=POLLIN|POLLERR;
+
+    while(EndFlag==0) {
+        switch((nready=poll(targets,1,1000))) {
+            case -1:
+                if(errno!=EINTR){
+                    perror("poll");
+                }
+                break;
+            case 0:
+                break;
+            default:
+                if(targets[0].revents&(POLLIN|POLLERR)) {
+                    if((len=read(DeviceSoc,buf,sizeof(buf)))<=0) {
+                        perror("read");
+                    } else {
+                        EtherRecv(DeviceSoc,buf,len);
+                    }
+                }
+                break;
+        }
+    }
+
+return (NULL);
+
+}
